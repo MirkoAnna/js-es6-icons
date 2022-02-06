1c7034a8d5c59d1950c47af4d305aa0f66153582
MirkoAnna <82913293+MirkoAnna@users.noreply.github.com>
2022-02-07 00:27:35 +0100

    Milestone 3

--- data.js
+++ data.js
@@ -1,7 +1,12 @@
 /*Milestone 1 
 Partendo dalla struttura dati fornita, visualizzare in pagina un box per ogni icona, in cui è presente il nome dell’icona e l’icona stessa. 
+
 Milestone 2 
-Ciascuna icona ha una proprietà “color”: utilizzare questa proprietà per visualizzare le icone del colore corrispondente.*/
+Ciascuna icona ha una proprietà “color”: utilizzare questa proprietà per visualizzare le icone del colore corrispondente.
+
+Milestone 3
+Aggiungere alla pagina una select in cui le options corrispondono ai vari tipi di icone (animal, vegetable, user).
+Quando l’utente seleziona un tipo dalla select, visualizzare solamente le icone corrispondenti.*/
 
 const icone = [
 	{
@@ -121,9 +126,32 @@ const icone = [
 const contenitoreBoxIcons = document.getElementById('boxes-container');
 creatoreIcone(contenitoreBoxIcons, icone);
 
+// Assegno il select ad una variabile
+const selettoreFiltro = document.getElementById('filter-select');
 
+// Assegno al select l'evento change
+selettoreFiltro.addEventListener('change', function () {
 
+	//NOTA: Posso utilizzare this perchè non è un arrow function
+	let selezionato = this.value;
+	// Se viene selezionato all visualizzo su schermo tuttti gli elementi
+	if (selezionato == "all") {
+		creatoreIcone(contenitoreBoxIcons, icone);
+	} 
+	// Visualizzo su schermo sole le icone corrispondenti utilizzando il metodo filter
+	else {
+		const iconeFiltrate = icone.filter(icona => {
+			
+			if (icona.type == selezionato) {
+				return true;
+			}
+			return false;
+		});
+		
+		creatoreIcone(contenitoreBoxIcons, iconeFiltrate);		
+	}
 
+});
 
 
 
